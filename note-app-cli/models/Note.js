146cc646ece229f232442988f617f1f4c4ab5e28
yasiqb89 <mian.yassir@gmail.com>
2025-09-19 22:02:36 +0200

    Note class added

---
+++ note-app-cli/models/Note.js
@@ -0,0 +1,31 @@
+class Note {
+    constructor(id, title, body, tags = [], createdAt = new Date()) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.tags = tags;
+        this.createdAt = new Date(createdAt);
+    }
+
+    getInfo() {
+        return `${this.id}. ${this.title} [${this.tags.join(", ")}]`
+    }
+
+    matchesKeyword(keyword) {
+        const lower = keyword.toLowerCase();
+        return (
+            this.title.toLowerCase().includes(lower) || this.body.toLowerCase().includes(lower)
+        );
+    }
+
+    hastTag(tag) {
+        return this.tags.includes(tag);
+    }
+
+    static parse(obj) {
+        return new Note(obj.id, obj.title, obj.body, obj.tags, obj.createdAt);
+    }
+
+}
+
+export default Note;
